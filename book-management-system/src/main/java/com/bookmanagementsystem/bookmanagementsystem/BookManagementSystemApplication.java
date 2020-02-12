d0003411727872d0f61a09196f10dfe35f67720f
promise <promiseokosun@gmail.com>
2020-02-13 00:17:56 +0100

    Start of the Book Management System Using TDD Approach

--- book-management-system/src/main/java/com/bookmanagementsystem/bookmanagementsystem/BookManagementSystemApplication.java
+++ book-management-system/src/main/java/com/bookmanagementsystem/bookmanagementsystem/BookManagementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookManagementSystemApplication.class, args);
+		SpringApplication.run(BookManagementSystemApplication.class, args);  
 	}
 
 }
